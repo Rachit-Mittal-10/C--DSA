bff3b740dc2595cbf844df7e1dfbd2f6b04b9f1d
Rachit Mittal <mittalrachit10063003@gmail.com>
2024-07-06 03:06:04 +0530

    Few Changes

--- PracticeQuestions/fibonacciDP.cpp
+++ PracticeQuestions/fibonacciDP.cpp
@@ -1,4 +1,6 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<chrono>
 using namespace std;
 /*
 Time Complexity: O(N)
@@ -6,11 +8,17 @@ Space Complexity: O(N)
 
 Reduced Function Calls
 */
+
+// Recursive Function with Memorization
+// Maximum Value of N: 4950
 int fibonacci(int n, vector<int>& memory){
+    // Base Case
     if(n <= 1){
         memory[n] = n;
         return n;
     }
+    // We check if we have value stored
+    // if not then value calculated
     int n1, n2;
     if(memory[n-1] != -1){
         n1 = memory[n-1];
@@ -27,6 +35,7 @@ int fibonacci(int n, vector<int>& memory){
     return fib;
 }
 
+// Recursive Function
 int Fibonacci(int n){
     if(n<=1){
         return n;
@@ -35,8 +44,11 @@ int Fibonacci(int n){
 }
 
 int main(){
-    int n = 60;
+    int n;
+    cout << "Enter the n: ";
+    cin >> n;
     vector<int> memory(n+1,-1);
+    /*
     chrono::high_resolution_clock::time_point start, end;
     chrono::duration<double> duration;
     start = chrono::high_resolution_clock::now();
@@ -49,5 +61,7 @@ int main(){
     end = chrono::high_resolution_clock::now();
     duration = end - start;
     cout << duration.count() << endl;
+    */
+    cout << "fibonacci of " << n <<  ": " << fibonacci(n, memory) << endl;
     return 0;
 }
