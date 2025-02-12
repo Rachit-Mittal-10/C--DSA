d4970dcf37e298d7ca2b879b99c98ada0f3c59fa
Rachit Mittal <mittalrachit10063003@gmail.com>
2025-02-12 22:47:19 +0530

    removed exe since redundant

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ if [ $# -eq 1 ]; then
 	if [ -e "$1" ]; then
 		filename=$(basename $1 .cpp)
 		mkdir -p ./bin
-		g++ "$1" -o "./bin/$filename.exe"
-		if [ -e "./bin/$filename.exe" ]; then
-			"./bin/$filename.exe"
+		g++ "$1" -o "./bin/$filename"
+		if [ -e "./bin/$filename" ]; then
+			"./bin/$filename"
 		fi
 	else
 		echo "No File Exist"
