fd7a97e1a096121403ca82323deadd041e26568e
Rachit Mittal <mittalrachit10063003@gmail.com>
2025-02-16 14:01:29 +0530

    clean updated

--- clean.sh
+++ clean.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-if [ "$(ls -A ./bin)" ]; then
-  rm -r ./bin/*
+if [ -d "./bin" ]; then
+  if [ "$(ls -A ./bin)" ]; then
+    rm -r ./bin/*
+  else
+    echo "The bin directory is already empty."
+  fi
 else
-  echo "The bin directory is already empty."
+  echo "The bin directory does not exist."
 fi
