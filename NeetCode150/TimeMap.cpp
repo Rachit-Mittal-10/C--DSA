992d632845095a38cef2581fc127f938253ab727
Rachit Mittal <mittalrachit10063003@gmail.com>
2025-03-20 22:13:02 +0530

    leetcode 981 done

--- NeetCode150/TimeMap.cpp
+++ NeetCode150/TimeMap.cpp
@@ -5,14 +5,18 @@
 using namespace std;
 
 class TimeMap {
-    unordered_map<string,vector<pair<string,int>>> mp;
+/*
+todo: Create a time based key value store
+- set is always strictly increasing timestamp ==> this is the sign of binary search
+*/
+    unordered_map<string,vector<pair<int,string>>> mp;
 public:
     TimeMap() {
         
     }
     
     void set(string key, string value, int timestamp) {
-        mp[key].push_back(make_pair(value,timestamp));
+        mp[key].push_back(make_pair(timestamp,value));
     }
     
     string get(string key, int timestamp) {
@@ -22,30 +26,46 @@ public:
         auto vectorKey = mp[key];
         int start = 0;
         int end = vectorKey.size()-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(vectorKey[mid].second == timestamp){
-                return vectorKey[mid].first;
+        // int index = -1;
+        // for(int i=0;i<=end;i++){
+        //     if(vectorKey[i].first <= timestamp){
+        //         index = i;
+        //     }
+        // }
+        // if(index == -1){
+        //     return "";
+        // }
+        // return vectorKey[index].second;
+        int mid = -1;
+        pair<int,string> curr;
+        string result = "";
+        while(start <= end){
+            mid = (end-start)/2 + start;
+            curr = vectorKey[mid];
+            if(curr.first > timestamp){
+                end = mid - 1;
             }
-            else if(vectorKey[mid].second > timestamp){
-                end=mid-1;
+            else if(curr.first < timestamp){
+                start = mid + 1;
+                result = curr.second;
             }
             else{
-                start=mid+1;
+                return curr.second;
             }
         }
-        if(end>=0){
-            return vectorKey[end].first;
-        }
-        return "";
+        return result;
     }
 };
+
 int main(void){
     auto tmp = TimeMap();
-    cout << tmp.get("foo",1) << endl;
     tmp.set("foo","bar",1);
+    cout << tmp.get("foo",1) << endl;
     cout << tmp.get("foo",3) << endl;
     tmp.set("foo","bar2",4);
+    /*
+    : "foo": [<1,"bar">,<4,"bar2">]
+    */
     cout << tmp.get("foo",4) << endl;
-    cout << tmp.get("foo",2) << endl;
+    cout << tmp.get("foo",5) << endl;
 }
