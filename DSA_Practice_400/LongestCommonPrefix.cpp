587bfb73a6629d0883ee7d0a10d8a11d88d948e3
Rachit Mittal <mittalrachit10063003@gmail.com>
2025-03-29 17:48:14 +0530

    longest common prefix done

--- DSA_Practice_400/LongestCommonPrefix.cpp
+++ DSA_Practice_400/LongestCommonPrefix.cpp
@@ -3,15 +3,27 @@ using namespace std;
 
 class BruteForce{
 public:
-    string solve(vector<int>& arr){
-        string resultPrefix = "";
-        int i=0;
-        
+    string solve(vector<string>& arr){
+        sort(arr.begin(),arr.end());
+        int n = arr.size();
+        string s1 = arr[0];
+        string s2 = arr[n-1];
+        string result = "";
+        int n2 = s1.size();
+        for(int i=0;i<n2;i++){
+            if(s1[i] != s2[i]){
+                break;
+            }
+            result += s1[i];
+        }
+        return result;
     }
 };
 
 
 int main(void){
-    
+    auto S = BruteForce();
+    vector<string> arr = {"flying","flea","flower"};
+    cout << S.solve(arr) << endl;
     return 0;
 }
